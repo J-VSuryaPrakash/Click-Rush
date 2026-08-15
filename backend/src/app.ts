7aaea4e40b668e6d2279fed7a565235b34f90940
Surya Prakash <surya.jinagam@gmail.com>
2026-08-15 21:27:17 +0530

    feat: update CORS origin URL by removing trailing slash for consistency

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -10,7 +10,7 @@ const httpServer = http.createServer(app);
 dotenv.config();
 app.use(express.json());
 app.use(cors({
-    origin: 'https://click-rush-silk.vercel.app/',
+    origin: 'https://click-rush-silk.vercel.app',
     credentials: true
 }));
 app.use(cookieParser());
